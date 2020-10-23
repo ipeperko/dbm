e64963573971b07866078cb18c86a617a4634374
ivo <ivo.peperko@i13tech.com>
2020-10-23 20:18:53 +0200

    Model query helper outside of class function definitions

--- include/detail/model_query_helper.hpp
+++ include/detail/model_query_helper.hpp
@@ -34,197 +34,217 @@ public:
     {
     }
 
-    std::string write_query() const
-    {
-        std::string keys, values, duplicate_keys;
-        int i = 0, i_duplicate = 0;
+    [[nodiscard]] std::string write_query() const;
 
-        for (const auto& it : model_.items()) {
+    [[nodiscard]] std::string read_query(const std::string& extra_condition) const;
 
-            if (it.direction() == kind::direction::read_only) {
-                continue;
+    [[nodiscard]] std::string delete_query() const;
+
+    [[nodiscard]] std::string create_table_query(bool if_not_exists) const;
+
+    [[nodiscard]] std::string drop_table_query(bool if_exists) const;
+};
+
+template<class SessionType>
+DBM_INLINE
+std::string model_query_helper<SessionType>::write_query() const
+{
+    std::string keys, values, duplicate_keys;
+    int i = 0, i_duplicate = 0;
+
+    for (const auto& it : model_.items()) {
+
+        if (it.direction() == kind::direction::read_only) {
+            continue;
+        }
+
+        if (it.is_defined()) {
+
+            if (i) {
+                keys += ",";
+                values += ",";
+            }
+            if (i_duplicate && !it.is_primary()) {
+                duplicate_keys += ",";
             }
 
-            if (it.is_defined()) {
-
-                if (i) {
-                    keys += ",";
-                    values += ",";
-                }
-                if (i_duplicate && !it.is_primary()) {
-                    duplicate_keys += ",";
-                }
-
-                keys += it.key().get();
-                if (it.is_null()) {
-                    values += "NULL";
-                }
-                else {
-                    values += "'" + it.to_string() + "'";
-                }
-
-                if (!it.is_primary()) {
-                    duplicate_keys += it.key().get() + "=VALUES(" + it.key().get() + ")";
-                    i_duplicate++;
-                }
-
-                ++i;
+            keys += it.key().get();
+            if (it.is_null()) {
+                values += "NULL";
             }
             else {
-                if (it.is_required()) {
-                    std::domain_error("Item is required " + it.key().get());
-                }
+                values += "'" + it.to_string() + "'";
             }
-        }
-
-        std::string q = "INSERT ";
-
-        if constexpr (is_SQlite()) {
-            if (model_.ignore_insert_) {
-                q += "OR IGNORE ";
-            }
-            else if (model_.update_duplicates_) {
-                q += "OR REPLACE ";
-            }
-        }
-        else if constexpr (is_MySql()) {
-            if (model_.ignore_insert_) {
-                q += "IGNORE ";
-            }
-        }
-
-        q += "INTO " + model_.table_
-             + " (" + keys + ") VALUES (" + values + ")";
-
-        if constexpr (is_MySql()) {
-            if (model_.update_duplicates_ && !duplicate_keys.empty()) {
-                q += " ON DUPLICATE KEY UPDATE " + duplicate_keys;
-            }
-        }
-
-        return q;
-    }
-
-    std::string read_query(const std::string& extra_condition) const
-    {
-        std::string q = "SELECT " ;
-        std::string what;
-
-        // keys
-        int n = 0;
-        for (const auto& it : model_.items_) {
-            if (it.direction() != kind::direction::write_only) {
-                if (n) {
-                    what += ",";
-                }
-
-                what += it.key().get();
-                ++n;
-            }
-        }
-
-        q += what + " FROM " + model_.table_ + " WHERE ";
-
-        // primary
-        n = 0;
-        for (const auto& it : model_.items_) {
-            // TODO: write only direction
-            if (!it.is_primary()) {
-                continue;
-            }
-            if (!it.is_defined()) {
-                throw_exception<std::domain_error>("Item " + it.key().get() + " not set");
-            }
-
-            if (n) {
-                q += " AND ";
-            }
-
-            q += it.key().get() + "='" + it.to_string() + "'";
-            ++n;
-        }
-
-        if (!extra_condition.empty()) {
-            q += " AND " + extra_condition;
-        }
-
-        return q;
-    }
-
-    std::string delete_query() const
-    {
-        std::string q = "DELETE FROM " + model_.table_ + " WHERE ";
-
-        int n = 0;
-        for (const auto& it : model_.items_) {
 
             if (!it.is_primary()) {
-                continue;
-            }
-            if (!it.is_defined()) {
-                std::domain_error("Item " + it.key().get() + " not set");
+                duplicate_keys += it.key().get() + "=VALUES(" + it.key().get() + ")";
+                i_duplicate++;
             }
 
-            if (n) {
-                q += " AND ";
-            }
-
-            q += it.key().get() + "='" + it.to_string() + "'";
-            ++n;
+            ++i;
         }
-
-        return q;
+        else {
+            if (it.is_required()) {
+                throw_exception<std::domain_error>("Item is required " + it.key().get());
+            }
+        }
     }
 
-    std::string create_table_query(bool if_not_exists) const
-    {
-        std::string q = "CREATE TABLE ";
-        if (if_not_exists) {
-            q += "IF NOT EXISTS ";
+    std::string q = "INSERT ";
+
+    if constexpr (is_SQlite()) {
+        if (model_.ignore_insert_) {
+            q += "OR IGNORE ";
         }
-        q += model_.table_name();
+        else if (model_.update_duplicates_) {
+            q += "OR REPLACE ";
+        }
+    }
+    else if constexpr (is_MySql()) {
+        if (model_.ignore_insert_) {
+            q += "IGNORE ";
+        }
+    }
 
-        std::string keys;
-        std::string primary_keys;
+    q += "INTO " + model_.table_
+         + " (" + keys + ") VALUES (" + values + ")";
 
-        for (const auto& it : model_.items()) {
+    if constexpr (is_MySql()) {
+        if (model_.update_duplicates_ && !duplicate_keys.empty()) {
+            q += " ON DUPLICATE KEY UPDATE " + duplicate_keys;
+        }
+    }
 
-            if (it.is_primary()) {
-                if (!primary_keys.empty()) {
-                    primary_keys += ", ";
-                }
-                primary_keys += it.key().get();
+    return q;
+}
+
+template<class SessionType>
+DBM_INLINE
+std::string model_query_helper<SessionType>::read_query(const std::string& extra_condition) const
+{
+    std::string q = "SELECT " ;
+    std::string what;
+
+    // keys
+    int n = 0;
+    for (const auto& it : model_.items_) {
+        if (it.direction() != kind::direction::write_only) {
+            if (n) {
+                what += ",";
             }
 
-            if (!it.dbtype().get().empty()) {
-                if (!keys.empty()) {
-                    keys += ", ";
-                }
-                keys += it.key().get() + " " + it.dbtype().get();
-            }
+            what += it.key().get();
+            ++n;
+        }
+    }
+
+    q += what + " FROM " + model_.table_ + " WHERE ";
+
+    // primary
+    n = 0;
+    for (const auto& it : model_.items_) {
+        // TODO: write only direction
+        if (!it.is_primary()) {
+            continue;
+        }
+        if (!it.is_defined()) {
+            throw_exception<std::domain_error>("Item " + it.key().get() + " not set");
         }
 
-        if (!keys.empty()) {
-            q += " (" + keys;
+        if (n) {
+            q += " AND ";
+        }
+
+        q += it.key().get() + "='" + it.to_string() + "'";
+        ++n;
+    }
+
+    if (!extra_condition.empty()) {
+        q += " AND " + extra_condition;
+    }
+
+    return q;
+}
+
+template<class SessionType>
+DBM_INLINE
+std::string model_query_helper<SessionType>::delete_query() const
+{
+    std::string q = "DELETE FROM " + model_.table_ + " WHERE ";
+
+    int n = 0;
+    for (const auto& it : model_.items_) {
+
+        if (!it.is_primary()) {
+            continue;
+        }
+        if (!it.is_defined()) {
+            throw_exception<std::domain_error>("Item " + it.key().get() + " not set");
+        }
+
+        if (n) {
+            q += " AND ";
+        }
+
+        q += it.key().get() + "='" + it.to_string() + "'";
+        ++n;
+    }
+
+    return q;
+}
+
+template<class SessionType>
+DBM_INLINE
+std::string model_query_helper<SessionType>::create_table_query(bool if_not_exists) const
+{
+    std::string q = "CREATE TABLE ";
+    if (if_not_exists) {
+        q += "IF NOT EXISTS ";
+    }
+    q += model_.table_name();
+
+    std::string keys;
+    std::string primary_keys;
+
+    for (const auto& it : model_.items()) {
+
+        if (it.is_primary()) {
             if (!primary_keys.empty()) {
-                q += ", PRIMARY KEY (" + primary_keys + ")";
+                primary_keys += ", ";
             }
-            q += ")";
+            primary_keys += it.key().get();
         }
 
-        return q;
+        if (!it.dbtype().get().empty()) {
+            if (!keys.empty()) {
+                keys += ", ";
+            }
+            keys += it.key().get() + " " + it.dbtype().get();
+        }
     }
 
-    std::string drop_table_query(bool if_exists) const
-    {
-        std::string q = "DROP TABLE ";
-        if (if_exists) {
-            q += "IF EXISTS ";
+    if (!keys.empty()) {
+        q += " (" + keys;
+        if (!primary_keys.empty()) {
+            q += ", PRIMARY KEY (" + primary_keys + ")";
         }
-        q += model_.table_name();
-        return q;
+        q += ")";
     }
-};
+
+    return q;
+}
+
+template<class SessionType>
+DBM_INLINE
+std::string model_query_helper<SessionType>::drop_table_query(bool if_exists) const
+{
+    std::string q = "DROP TABLE ";
+    if (if_exists) {
+        q += "IF EXISTS ";
+    }
+    q += model_.table_name();
+    return q;
+}
 
 } // namespace
 
