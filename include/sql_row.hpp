e30b34a6fb922c421387ff041f32bc85532a855a
ivo <ivo.peperko@i13tech.com>
2020-10-23 20:14:25 +0200

    Sql rows member functions outside of class definitions

--- include/sql_row.hpp
+++ include/sql_row.hpp
@@ -9,83 +9,90 @@ namespace dbm::kind {
 class sql_row : public std::vector<sql_value>
 {
 public:
-    sql_row()
-    {}
+    sql_row() = default;
 
-    void set_fields(kind::sql_fields* field_names, sql_field_map* field_map)
-    {
-        fnames_ = field_names;
-        fmap_ = field_map;
-    }
+    virtual ~sql_row() = default;
 
-    sql_value& at(std::string_view key)
-    {
-        for (auto& it : *fmap_) {
-            if (it.first == key) {
-                return at(it.second);
-            }
-        }
+    void set_fields(kind::sql_fields* field_names, sql_field_map* field_map);
 
-        throw_exception<std::out_of_range>("sql row item not found " + std::string(key));
-    }
+    sql_value& at(std::string_view key);
 
-    sql_value const& at(std::string_view key) const
-    {
-        for (auto& it : *fmap_) {
-            if (it.first == key) {
-                return at(it.second);
-            }
-        }
+    sql_value const& at(std::string_view key) const;
 
-        throw_exception<std::out_of_range>("sql row item not found " + std::string(key));
-    }
+    sql_value& at(int idx);
 
-    /*
-    sql_value& at(const kind::key& key)
-    {
-        return at(fmap_->at(key));
-    }
+    sql_value const& at(int idx) const;
 
-    sql_value const& at(const kind::key& key) const
-    {
-        return at(fmap_->at(key));
-    }
-     */
+    sql_fields* field_names();
 
-    sql_value& at(int idx)
-    {
-        return std::vector<sql_value>::at(idx);
-    }
+    sql_fields const* field_names() const;
 
-    sql_value const& at(int idx) const
-    {
-        return std::vector<sql_value>::at(idx);
-    }
+    sql_field_map* field_map();
 
-    sql_fields* field_names()
-    {
-        return fnames_;
-    }
-
-    sql_fields const* field_names() const
-    {
-        return fnames_;
-    }
-
-    sql_field_map* field_map()
-    {
-        return fmap_;
-    }
-
-    sql_field_map const* field_map() const
-    {
-        return fmap_;
-    }
+    sql_field_map const* field_map() const;
 
 private:
     sql_fields* fnames_ { nullptr };
     sql_field_map* fmap_ { nullptr };
 };
+
+DBM_INLINE void sql_row::set_fields(kind::sql_fields* field_names, sql_field_map* field_map)
+{
+    fnames_ = field_names;
+    fmap_ = field_map;
+}
+
+DBM_INLINE sql_value& sql_row::at(std::string_view key)
+{
+    for (auto& it : *fmap_) {
+        if (it.first == key) {
+            return at(it.second);
+        }
+    }
+
+    throw_exception<std::out_of_range>("sql row item not found " + std::string(key));
+}
+
+DBM_INLINE sql_value const& sql_row::at(std::string_view key) const
+{
+    for (auto& it : *fmap_) {
+        if (it.first == key) {
+            return at(it.second);
+        }
+    }
+
+    throw_exception<std::out_of_range>("sql row item not found " + std::string(key));
+}
+
+DBM_INLINE sql_value& sql_row::at(int idx)
+{
+    return std::vector<sql_value>::at(idx);
+}
+
+DBM_INLINE sql_value const& sql_row::at(int idx) const
+{
+    return std::vector<sql_value>::at(idx);
+}
+
+DBM_INLINE sql_fields* sql_row::field_names()
+{
+    return fnames_;
+}
+
+DBM_INLINE sql_fields const* sql_row::field_names() const
+{
+    return fnames_;
+}
+
+DBM_INLINE sql_field_map* sql_row::field_map()
+{
+    return fmap_;
+}
+
+DBM_INLINE sql_field_map const* sql_row::field_map() const
+{
+    return fmap_;
+}
 
 }
 
--- include/sql_rows_dump.hpp
+++ include/sql_rows_dump.hpp
@@ -11,111 +11,137 @@ class sql_rows_dump
 public:
     sql_rows_dump() = default;
 
-    sql_rows_dump(const sql_rows& src) // implicit is desired
-    {
-        store(src);
-    }
+    virtual ~sql_rows_dump() = default;
 
-    sql_rows_dump& operator=(const sql_rows& src)
-    {
-        store(src);
-        return *this;
-    }
+    sql_rows_dump(const sql_rows& src); // implicit is desired
 
-    sql_rows_dump& operator+=(const sql_rows& src)
-    {
-        if (src.field_map() != fmap_ && src.field_names() != fnames_) {
-            throw_exception<std::domain_error>("Cannot append rows with different field names");
-        }
-        append(src);
-        return *this;
-    }
+    sql_rows_dump& operator=(const sql_rows& src);
 
-    void store(const sql_rows& src)
-    {
-        data_.clear();
-        fnames_ = src.field_names();
-        fmap_ = src.field_map();
-        //        size_t n = src.size() * field_names_.size();
-        //        data_.reserve(n);
-        append(src);
-    }
+    sql_rows_dump& operator+=(const sql_rows& src);
 
-    void restore(sql_rows& dest)
-    {
-        dest.clear();
-        dest.set_field_names(sql_fields(fnames_));
+    void store(const sql_rows& src);
 
-        for (const auto& row : data_) {
-            auto& dest_row = dest.emplace_back();
-            for (const auto& val : row) {
-                if (val)
-                    dest_row.emplace_back(val->data(), val->size());
-                else
-                    dest_row.emplace_back(nullptr, 0);
-            }
-        }
+    void restore(sql_rows& dest);
 
-        dest.setup_rows();
-    }
+    sql_rows restore();
 
-    sql_rows restore()
-    {
-        sql_rows rows;
-        restore(rows);
-        return rows;
-    }
+    sql_fields const& field_names() const;
 
-    sql_fields const& field_names() const
-    {
-        return fnames_;
-    }
+    sql_field_map const& field_map() const;
 
-    sql_field_map const& field_map() const
-    {
-        return fmap_;
-    }
+    size_t size() const;
 
-    size_t size() const
-    {
-        return data_.size();
-    }
+    bool empty() const;
 
-    bool empty() const
-    {
-        return data_.empty();
-    }
-
-    void clear()
-    {
-        data_.clear();
-        data_.shrink_to_fit();
-        fnames_.clear();
-        fmap_.clear();
-    }
+    void clear();
 
 private:
-    void append(const sql_rows& src)
-    {
-        for (auto const& src_row : src) {
-            auto& row = data_.emplace_back();
-
-            for (auto const& src_val : src_row) {
-                if (src_val.null()) {
-                    row.emplace_back(std::nullopt);
-                }
-                else {
-                    std::string s(src_val.get().data(), src_val.length());
-                    row.emplace_back(std::move(s));
-                }
-            }
-        }
-    }
+    void append(const sql_rows& src);
 
     sql_fields fnames_;
     sql_field_map fmap_;
     std::vector<std::vector<std::optional<std::string>>> data_;
 };
+
+DBM_INLINE sql_rows_dump::sql_rows_dump(const sql_rows& src) // implicit is desired
+{
+    store(src);
+}
+
+DBM_INLINE sql_rows_dump& sql_rows_dump::operator=(const sql_rows& src)
+{
+    store(src);
+    return *this;
+}
+
+DBM_INLINE sql_rows_dump& sql_rows_dump::operator+=(const sql_rows& src)
+{
+    if (src.field_map() != fmap_ && src.field_names() != fnames_) {
+        throw_exception<std::domain_error>("Cannot append rows with different field names");
+    }
+    append(src);
+    return *this;
+}
+
+DBM_INLINE void sql_rows_dump::store(const sql_rows& src)
+{
+    data_.clear();
+    fnames_ = src.field_names();
+    fmap_ = src.field_map();
+    //        size_t n = src.size() * field_names_.size();
+    //        data_.reserve(n);
+    append(src);
+}
+
+DBM_INLINE void sql_rows_dump::restore(sql_rows& dest)
+{
+    dest.clear();
+    dest.set_field_names(sql_fields(fnames_));
+
+    for (const auto& row : data_) {
+        auto& dest_row = dest.emplace_back();
+        for (const auto& val : row) {
+            if (val)
+                dest_row.emplace_back(val->data(), val->size());
+            else
+                dest_row.emplace_back(nullptr, 0);
+        }
+    }
+
+    dest.setup_rows();
+}
+
+DBM_INLINE sql_rows sql_rows_dump::restore()
+{
+    sql_rows rows;
+    restore(rows);
+    return rows;
+}
+
+DBM_INLINE sql_fields const& sql_rows_dump::field_names() const
+{
+    return fnames_;
+}
+
+DBM_INLINE sql_field_map const& sql_rows_dump::field_map() const
+{
+    return fmap_;
+}
+
+DBM_INLINE size_t sql_rows_dump::size() const
+{
+    return data_.size();
+}
+
+DBM_INLINE bool sql_rows_dump::empty() const
+{
+    return data_.empty();
+}
+
+DBM_INLINE void sql_rows_dump::clear()
+{
+    data_.clear();
+    data_.shrink_to_fit();
+    fnames_.clear();
+    fmap_.clear();
+}
+
+DBM_INLINE void sql_rows_dump::append(const sql_rows& src)
+{
+    for (auto const& src_row : src) {
+        auto& row = data_.emplace_back();
+
+        for (auto const& src_val : src_row) {
+            if (src_val.null()) {
+                row.emplace_back(std::nullopt);
+            }
+            else {
+                std::string s(src_val.get().data(), src_val.length());
+                row.emplace_back(std::move(s));
+            }
+        }
+    }
+}
 
 }
 
